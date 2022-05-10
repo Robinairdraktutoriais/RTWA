b45ea6a6b15887acd4b4cacc33ca0c3010d833fb
Robinairdraktutoriais <81757745+Robinairdraktutoriais@users.noreply.github.com>
2022-05-10 16:24:42 -0300

    Add files via upload

---
+++ clogs.js
@@ -0,0 +1,20 @@
+{
+    "pacotes": [
+        {   
+            "versão": "19.35",
+            "pacote": "com.whatsapp",
+            "status": "update_title",
+            "title": "WhatsApp {M Pro} 19.35",
+            "nota": "falso",
+            "msgtxt": "https://raw.githubusercontent.com/CrisMod/WhatsApp/main/changelog-en.html",
+            "baixar": "https://crismod.com/whatsapp/com.whatsapp",
+            "atualizar": [
+                {
+                    "languaje": "pt",
+                    "tema": "escuro",
+                    "changelog": "https://raw.githubusercontent.com/CrisMod/up_system/main/MPro/dark/changelog-en.html"
+                },
+                {
+                }
+            ]
+        },
