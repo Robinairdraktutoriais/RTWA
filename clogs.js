a319b17fe60085b4a55d50759e3c99a50f781aa3
Robinairdraktutoriais <81757745+Robinairdraktutoriais@users.noreply.github.com>
2022-05-10 17:31:19 -0300

    Update clogs.js

--- clogs.js
+++ clogs.js
@@ -1,10 +1,10 @@
 {
     "pacotes": [
         {   
-            "versão": "16.25",
+            "versão": "16.20",
             "pacote": "com.whatsapp",
             "status": "update_title",
-            "title": "WhatsApp plus 16.25",
+            "title": "WhatsApp plus 16.20",
             "nota": "falso",
             "msgtxt": "https://raw.githubusercontent.com/Robinairdraktutoriais/RTWA/main/Mais/changelog-pt.txt",
             "baixar": "https://youtube.com/c/RobinAirdraktutoriais",
